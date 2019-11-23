5b3288f3939e415334bb32c8fe6ab9919b0031e5
johnnyshields <johnny.shields@gmail.com>
2019-11-24 03:15:07 +0900

    Fix more specs

--- lib/mongoid/relations_dirty_tracking.rb
+++ lib/mongoid/relations_dirty_tracking.rb
@@ -10,24 +10,17 @@ module Mongoid
 
     module ClassMethods
       def relations_dirty_tracking(options = {})
-        relations_dirty_tracking_options[:only] += [options[:only] || []].flatten.map(&:to_s)
+        relations_dirty_tracking_options[:only]   += [options[:only]   || []].flatten.map(&:to_s)
         relations_dirty_tracking_options[:except] += [options[:except] || []].flatten.map(&:to_s)
       end
 
       def track_relation?(rel_name)
         rel_name = rel_name.to_s
         options = relations_dirty_tracking_options
-        to_track = (!options[:only].blank? && options[:only].include?(rel_name)) \
-          || (options[:only].blank? && !options[:except].include?(rel_name))
+        to_track = (!options[:only].blank? && options[:only].include?(rel_name)) ||
+                   (options[:only].blank? && !options[:except].include?(rel_name))
 
-        to_track && [
-          Mongoid::Association::Embedded::EmbedsOne::Proxy,
-          Mongoid::Association::Embedded::EmbedsMany::Proxy,
-          Mongoid::Association::Referenced::HasOne::Proxy,
-          Mongoid::Association::Referenced::HasMany::Proxy,
-          Mongoid::Association::Referenced::HasAndBelongsToMany::Proxy,
-          Mongoid::Association::Referenced::BelongsTo::Proxy
-        ].include?(relations[rel_name].try(:relation))
+        to_track && Mongoid::RelationsDirtyTracking.trackable_proxies.include?(relations[rel_name].try(:relation))
       end
 
       def tracked_relations
@@ -36,112 +29,133 @@ module Mongoid
     end
 
     included do
-      after_initialize  :store_relations_shadow
-      after_save        :store_relations_shadow
+      after_initialize :store_relations_shadow
+      after_save       :store_relations_shadow
 
       cattr_accessor :relations_dirty_tracking_options
       self.relations_dirty_tracking_options = { only: [], except: ['versions'] }
-    end
 
-    def store_relations_shadow
-      @relations_shadow = {}
-      self.class.tracked_relations.each do |rel_name|
-        @relations_shadow[rel_name] = tracked_relation_attributes(rel_name)
+      def store_relations_shadow
+        @relations_shadow = {}
+        self.class.tracked_relations.each do |rel_name|
+          @relations_shadow[rel_name] = tracked_relation_attributes(rel_name)
+        end
       end
-    end
 
-    def relation_changes
-      changes = {}
-      @relations_shadow.each_pair do |rel_name, shadow_values|
-        current_values = tracked_relation_attributes(rel_name)
-        changes[rel_name] = [shadow_values, current_values] if current_values != shadow_values
+      def relation_changes
+        changes = {}
+        @relations_shadow.each_pair do |rel_name, shadow_values|
+          current_values = tracked_relation_attributes(rel_name)
+          changes[rel_name] = [shadow_values, current_values] if current_values != shadow_values
+        end
+        changes
       end
-      changes
-    end
 
-    def relations_changed?
-      !relation_changes.empty?
-    end
+      def relations_changed?
+        !relation_changes.empty?
+      end
 
-    def changed_with_relations?
-      changed? || relations_changed?
-    end
+      def changed_with_relations?
+        changed? || relations_changed?
+      end
 
-    def changes_with_relations
-      (changes || {}).merge relation_changes
-    end
+      def changes_with_relations
+        (changes || {}).merge relation_changes
+      end
 
-    def tracked_relation_attributes(rel_name)
-      rel_name = rel_name.to_s
-      meta = relations[rel_name]
-      return nil unless meta
-      case meta
-      when association_embeds_one
-        val = send(rel_name)
-        val && val.attributes.clone.delete_if { |key, _| key == 'updated_at' }
-      when association_embeds_many
-        val = send(rel_name)
-        val && val.map { |child| child.attributes.clone.delete_if { |key, _| key == 'updated_at' } }
-      when association_has_one
-        send(rel_name) && { meta.key.to_s => send(rel_name)[meta.key] }
-      when association_has_many
-        send(rel_name).map { |child| { meta.key.to_s => child.id } }
-      when association_habtm
-        send(rel_name).map { |child| { meta.primary_key.to_s => child.id } }
-      when association_belongs_to
-        begin
-          send(meta.foreign_key) && { meta.foreign_key.to_s => send(meta.foreign_key) }
-        rescue ActiveModel::MissingAttributeError
-          nil
+      def tracked_relation_attributes(rel_name)
+        rel_name = rel_name.to_s
+        meta = relations[rel_name]
+        return nil unless meta
+        case meta
+        when Mongoid::RelationsDirtyTracking.association_embeds_one
+          val = send(rel_name)
+          val && val.attributes.clone.delete_if { |key, _| key == 'updated_at' }
+        when Mongoid::RelationsDirtyTracking.association_embeds_many
+          val = send(rel_name)
+          val && val.map { |child| child.attributes.clone.delete_if { |key, _| key == 'updated_at' } }
+        when Mongoid::RelationsDirtyTracking.association_has_one
+          send(rel_name) && { meta.key.to_s => send(rel_name)[meta.key] }
+        when Mongoid::RelationsDirtyTracking.association_has_many
+          send(rel_name).map { |child| { meta.key.to_s => child.id } }
+        when Mongoid::RelationsDirtyTracking.association_habtm
+          send(rel_name).map { |child| { meta.primary_key.to_s => child.id } }
+        when Mongoid::RelationsDirtyTracking.association_belongs_to
+          begin
+            send(meta.foreign_key) && { meta.foreign_key.to_s => send(meta.foreign_key) }
+          rescue ActiveModel::MissingAttributeError
+            nil
+          end
         end
       end
     end
 
-    def association_belongs_to
-      if defined?(Mongoid::Association)
-        Mongoid::Association::Referenced::BelongsTo
-      else
-        Mongoid::Relations::Referenced::In
+    class << self
+      def association_belongs_to
+        if defined?(Mongoid::Association)
+          Mongoid::Association::Referenced::BelongsTo
+        else
+          Mongoid::Relations::Referenced::In
+        end
       end
-    end
 
-    def association_has_one
-      if defined?(Mongoid::Association)
-        Mongoid::Association::Referenced::HasOne
-      else
-        Mongoid::Relations::Referenced::One
+      def association_has_one
+        if defined?(Mongoid::Association)
+          Mongoid::Association::Referenced::HasOne
+        else
+          Mongoid::Relations::Referenced::One
+        end
       end
-    end
 
-    def association_has_many
-      if defined?(Mongoid::Association)
-        Mongoid::Association::Referenced::HasMany
-      else
-        Mongoid::Relations::Referenced::Many
+      def association_has_many
+        if defined?(Mongoid::Association)
+          Mongoid::Association::Referenced::HasMany
+        else
+          Mongoid::Relations::Referenced::Many
+        end
       end
-    end
 
-    def association_habtm
-      if defined?(Mongoid::Association)
-        Mongoid::Association::Referenced::HasAndBelongsToMany
-      else
-        Mongoid::Relations::Referenced::ManyToMany
+      def association_habtm
+        if defined?(Mongoid::Association)
+          Mongoid::Association::Referenced::HasAndBelongsToMany
+        else
+          Mongoid::Relations::Referenced::ManyToMany
+        end
       end
-    end
 
-    def association_embeds_one
-      if defined?(Mongoid::Association)
-        Mongoid::Association::Embedded::EmbedsOne
-      else
-        Mongoid::Relations::Embedded::One
+      def association_embeds_one
+        if defined?(Mongoid::Association)
+          Mongoid::Association::Embedded::EmbedsOne
+        else
+          Mongoid::Relations::Embedded::One
+        end
       end
-    end
 
-    def association_embeds_many
-      if defined?(Mongoid::Association)
-        Mongoid::Association::Embedded::EmbedsMany
-      else
-        Mongoid::Relations::Embedded::Many
+      def association_embeds_many
+        if defined?(Mongoid::Association)
+          Mongoid::Association::Embedded::EmbedsMany
+        else
+          Mongoid::Relations::Embedded::Many
+        end
+      end
+
+      def trackable_associations
+        @trackable_associations ||= [association_belongs_to,
+                                     association_has_one,
+                                     association_has_many,
+                                     association_habtm,
+                                     association_embeds_one,
+                                     association_embeds_many]
+      end
+
+      def trackable_proxies
+        @trackable_proxies ||= begin
+          if defined?(Mongoid::Association)
+            trackable_associations.map { |mod| mod::Proxy }
+          else
+            trackable_associations
+          end
+        end
       end
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,19 +8,25 @@ require 'mongoid/relations_dirty_tracking'
 require 'rspec'
 require 'rspec/its'
 
-if Mongoid::Config.respond_to?(:belongs_to_required_by_default)
-  Mongoid::Config.belongs_to_required_by_default = false
+Mongoid.configure do |config|
+  config.connect_to('mongoid_relations_dirty_tracking_test')
+  config.belongs_to_required_by_default = false if config.respond_to?(:belongs_to_required_by_default)
+end
+
+RSpec.configure do |config|
+  config.mock_with :rspec
+  config.after(:each) { Mongoid.purge! }
 end
 
 class TestDocument
   include Mongoid::Document
   include Mongoid::RelationsDirtyTracking
 
-  embeds_one  :one_document,    class_name: 'TestEmbeddedDocument'
-  embeds_many :many_documents,  class_name: 'TestEmbeddedDocument'
+  embeds_one  :one_document,   class_name: 'TestEmbeddedDocument'
+  embeds_many :many_documents, class_name: 'TestEmbeddedDocument'
 
-  has_one     :one_related,     class_name: 'TestRelatedDocument'
-  has_many    :many_related,    class_name: 'TestRelatedDocument'
+  has_one     :one_related,    class_name: 'TestRelatedDocument'
+  has_many    :many_related,   class_name: 'TestRelatedDocument'
   has_and_belongs_to_many :many_to_many_related, class_name: 'TestRelatedDocument'
 end
 
@@ -59,13 +65,4 @@ class TestDocumentWithExceptOption
   has_one     :one_related,     class_name: 'TestRelatedDocument'
 
   relations_dirty_tracking except: 'many_documents'
-end
-
-Mongoid.configure do |config|
-  config.connect_to('mongoid_relations_dirty_tracking_test')
-end
-
-RSpec.configure do |config|
-  config.mock_with :rspec
-  config.after(:each) { Mongoid.purge! }
 end
